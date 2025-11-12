c4bb83f5d99bb672f1279514bd599552992bf35d
kc-clintone <kayseeclintone@gmail.com>
2025-11-12 14:26:06 +0300

    added function to fetch the products

---
+++ fetchProducts.js
@@ -0,0 +1,15 @@
+const PRODUCT_API = "https://0b348e0c-69af-4ca2-b539-a28fc92c5b05.mock.pstmn.io/products";
+
+async function fetchAndFilterProducts() {
+  try {
+    const data = await fetch(PRODUCT_API);
+    if (!data.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+
+    let products = await data.json();
+
+  } catch (err) {
+    console.error("Failed to fetch products:", err);
+  }
+}
+
+fetchAndFilterProducts();
